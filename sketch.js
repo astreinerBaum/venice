23b613db13499f8f8933b63e1fac82b12d8c40c9
Matthias Pitscher <schaefer9111@gmail.com>
2017-07-07 23:27:50 +0200

    rev 3

--- sketch.js
+++ sketch.js
@@ -1,29 +1,23 @@
 var triggered = false;
 var now, end;
 var counter = 0;
+var videolength = 5 - 1;
 var ani;
-
-
-var vid = [
-  "material/water.mp4",
-  "material/marco.mp4",
-  "material/aperol.mp4",
-  "material/pigeon.mp4"
-];
+var vid;
 
 var gif = [
-  "material/water.gif",
-  "material/marco.gif",
-  "material/aperol.gif",
-  "material/pigeon.gif"
+    "material/water.gif",
+    "material/bridge.gif",
+    "material/marco.gif",
+    "material/aperol.gif",
+    "material/pigeon.gif"
 ];
 
 function setup() {
   noCanvas();
-  var bg = select('#background');
-  ani = select('#forground');
-  //bg.src = vid[2];
-
+  ani = select("#forground");
+  vid = select("#" + counter.toString());
+  vid.show();
 }
 
 function draw() {
@@ -31,25 +25,20 @@ function draw() {
   now = millis();
 
   if(triggered == true && end-now <= 0){
+    var ani = select('#forground');
     ani.attribute('src', '');
     triggered = false;
   }
 
 }
 
-function mousePressed(){
 
+function trigger(){
   if (triggered == false){
+    var ani = select('#forground');
     ani.attribute('src', gif[counter]);
+    releaseGif(5000);
   }
-
-}
-
-
-function mouseReleased(){
-
-  releaseGif(10000);
-
 }
 
 function releaseGif(milliseconds){
@@ -57,4 +46,38 @@ function releaseGif(milliseconds){
     end = millis() + milliseconds;
     triggered = true;
   }
+
+}
+
+function left(){
+  if (counter > 0){
+    vid = select("#" + counter.toString());
+    vid.hide();
+    counter--;
+    vid = select("#" + counter.toString());
+    vid.show();
+  }else{
+    vid = select("#" + counter.toString());
+    vid.hide();
+    counter = videolength;
+    vid = select("#" + counter.toString());
+    vid.show();
+  }
+}
+
+function right(){
+  if (counter < videolength){
+    vid = select("#" + counter.toString());
+    vid.hide();
+    counter++;
+    vid = select("#" + counter.toString());
+    vid.show();
+  }else{
+    vid = select("#" + counter.toString());
+    vid.hide();
+    counter = 0;
+    vid = select("#" + counter.toString());
+    vid.show();
+  }
+
 }
