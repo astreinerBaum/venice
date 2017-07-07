75803a21485b9375d9344eacb933dd10b644a7f8
Matthias Pitscher <schaefer9111@gmail.com>
2017-07-07 21:34:25 +0200

    rev2

--- sketch.js
+++ sketch.js
@@ -1,18 +1,60 @@
+var triggered = false;
+var now, end;
+var counter = 0;
+var ani;
+
+
+var vid = [
+  "material/water.mp4",
+  "material/marco.mp4",
+  "material/aperol.mp4",
+  "material/pigeon.mp4"
+];
+
+var gif = [
+  "material/water.gif",
+  "material/marco.gif",
+  "material/aperol.gif",
+  "material/pigeon.gif"
+];
+
 function setup() {
+  noCanvas();
+  var bg = select('#background');
+  ani = select('#forground');
+  //bg.src = vid[2];
 
 }
 
 function draw() {
 
+  now = millis();
+
+  if(triggered == true && end-now <= 0){
+    ani.attribute('src', '');
+    triggered = false;
+  }
+
 }
 
 function mousePressed(){
-  var animation = select('.forground');
-  animation.show();
-  console.log(animation.attribute("opacity"))
+
+  if (triggered == false){
+    ani.attribute('src', gif[counter]);
+  }
+
 }
 
+
 function mouseReleased(){
-  var animation = select('.forground');
-  animation.hide();
+
+  releaseGif(10000);
+
+}
+
+function releaseGif(milliseconds){
+  if (triggered == false){
+    end = millis() + milliseconds;
+    triggered = true;
+  }
 }
